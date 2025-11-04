c750067a400724e8097941ffcc1646a35e24e682
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-04 13:41:00 -0300

    feat: Atualiza layout da página NoteList com novos estilos e componentes

--- src/pages/NoteList.tsx
+++ src/pages/NoteList.tsx
@@ -1,14 +1,23 @@
+import Button from "../components/Button";
+import Navbar from "../components/Navbar";
+import { Plus } from 'lucide-react';
+
 export default function NoteList() {
   return (
     <div className="flex flex-col w-dvw h-dvh">
-      <header className="flex items-center h-2/12 px-16">
+      <header className="flex items-center h-3/14 px-20">
         <h1 className="text-3xl">Cleernote</h1>
       </header>
-      <main className="h-8/10 px-16">
+      <main className="h-8/10 px-20">
         <p>Here is a list of notes.</p>
       </main>
-      <footer className="h-2/12 px-16">
-        <p>Navbar</p>
+      <footer className="flex justify-center items-center h-3/14 px-20">
+        <Navbar>
+          <Button>
+            <Plus />
+            New note
+          </Button>
+        </Navbar>
       </footer>
     </div>
   )
