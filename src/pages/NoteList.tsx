c4337f3d5fea54a1561678b2a5e7e5bf92ab048a
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-03 23:32:31 -0300

    feat: Implementa estrutura básica para os elementos de NoteList

--- src/pages/NoteList.tsx
+++ src/pages/NoteList.tsx
@@ -1,7 +1,15 @@
 export default function NoteList() {
   return (
-    <>
-      <h1 className="text-blue-600">Note List</h1>
-    </>
+    <div className="flex flex-col w-dvw h-dvh">
+      <header className="flex items-center h-2/12 px-16">
+        <h1 className="text-3xl">Cleernote</h1>
+      </header>
+      <main className="h-8/10 px-16">
+        <p>Here is a list of notes.</p>
+      </main>
+      <footer className="h-2/12 px-16">
+        <p>Navbar</p>
+      </footer>
+    </div>
   )
 }
