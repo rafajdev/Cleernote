46981c30fd8c54bcd2732b29457eaf2b91bf0283
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-04 14:29:41 -0300

    feat/style: Implementa card ao componente NoteList e altera formatação do código

--- src/pages/NoteList.tsx
+++ src/pages/NoteList.tsx
@@ -1,24 +1,25 @@
-import Button from "../components/Button";
-import Navbar from "../components/Navbar";
+import Button from '../components/Button';
+import Card from '../components/Card';
+import Navbar from '../components/Navbar';
 import { Plus } from 'lucide-react';
 
 export default function NoteList() {
-  return (
-    <div className="flex flex-col w-dvw h-dvh">
-      <header className="flex items-center h-3/14 px-20">
-        <h1 className="text-3xl">Cleernote</h1>
-      </header>
-      <main className="h-8/10 px-20">
-        <p>Here is a list of notes.</p>
-      </main>
-      <footer className="flex justify-center items-center h-3/14 px-20">
-        <Navbar>
-          <Button>
-            <Plus />
-            New note
-          </Button>
-        </Navbar>
-      </footer>
-    </div>
-  )
+	return (
+		<div className='flex flex-col w-dvw h-dvh'>
+			<header className='flex items-center h-3/14 px-20'>
+				<h1 className='text-3xl font-semibold'>Cleernote</h1>
+			</header>
+			<main className='flex flex-wrap gap-10 h-8/10 px-20'>
+				<Card />
+			</main>
+			<footer className='flex justify-center items-center h-3/14 px-20'>
+				<Navbar>
+					<Button>
+						<Plus />
+						New note
+					</Button>
+				</Navbar>
+			</footer>
+		</div>
+	);
 }
