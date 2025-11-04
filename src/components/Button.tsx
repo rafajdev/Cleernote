f4121272e0443849835950b1f7dd7d297e99f595
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-04 14:28:16 -0300

    style: Alterando formatação do código

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,22 @@
-import type { ReactNode } from "react"
+import type { ReactNode } from 'react';
 
 type ButtonProps = {
-   children?: ReactNode
-}
+	children?: ReactNode;
+};
 
-export default function Button({children}: ButtonProps) {
-   return (
-      <button className="
+export default function Button({ children }: ButtonProps) {
+	return (
+		<button
+			className='
          flex items-center justify-center gap-2 
          w-52 h-10 
          text-white 
          bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 
          rounded-md 
          transition-colors 
-         cursor-pointer"
-      >
-         {children}
-      </button>
-   )
+         cursor-pointer'
+		>
+			{children}
+		</button>
+	);
 }
