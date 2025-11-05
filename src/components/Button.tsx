479eff084a8f253f7a737a3b186c150e0953b282
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-05 16:24:24 -0300

    style: Altera a cor do botão com icone para a anterior

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({ children, iconOnly }: ButtonProps) {
          flex items-center justify-center gap-2 
          ${iconOnly ? 'w-10' : 'w-52'} h-10 
          text-white 
-         ${iconOnly ? 'bg-gray-800 hover:bg-gray-900 focus:bg-gray-950' : 'bg-blue-500 hover:bg-blue-600 focus:bg-blue-700'} 
+         bg-blue-500 hover:bg-blue-600 focus:bg-blue-700
          rounded-md 
          transition-colors 
          cursor-pointer`}
