5afee9d5cf47b8e91306d0586584cd27f6b5f1f7
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-05 00:11:33 -0300

    feat: Implementa opção de botão apenas com icone e faz alteração nos estilos

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,19 +2,20 @@ import type { ReactNode } from 'react';
 
 type ButtonProps = {
 	children?: ReactNode;
+	iconOnly?: boolean;
 };
 
-export default function Button({ children }: ButtonProps) {
+export default function Button({ children, iconOnly }: ButtonProps) {
 	return (
 		<button
-			className='
+			className={`
          flex items-center justify-center gap-2 
-         w-52 h-10 
+         ${iconOnly ? 'w-10' : 'w-52'} h-10 
          text-white 
-         bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 
+         ${iconOnly ? 'bg-gray-800 hover:bg-gray-900 focus:bg-gray-950' : 'bg-blue-500 hover:bg-blue-600 focus:bg-blue-700'} 
          rounded-md 
          transition-colors 
-         cursor-pointer'
+         cursor-pointer`}
 		>
 			{children}
 		</button>
