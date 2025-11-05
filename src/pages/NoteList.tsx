1c2b083d2bb4366888967a16350bf41df43ed008
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-05 00:12:57 -0300

    feat: Incorpora icone a barra de navegação, botão de tema e componentes cards para teste

--- src/pages/NoteList.tsx
+++ src/pages/NoteList.tsx
@@ -2,6 +2,7 @@ import Button from '../components/Button';
 import Card from '../components/Card';
 import Navbar from '../components/Navbar';
 import { Plus } from 'lucide-react';
+import { SunMoon } from 'lucide-react';
 
 export default function NoteList() {
 	return (
@@ -11,12 +12,24 @@ export default function NoteList() {
 			</header>
 			<main className='flex flex-wrap gap-10 h-8/10 px-20'>
 				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
 			</main>
 			<footer className='flex justify-center items-center h-3/14 px-20'>
 				<Navbar>
+					<img src='/cleernote_icon.png' className='h-10'/>
 					<Button>
 						<Plus />
 						New note
+					</Button>
+					<Button iconOnly={true}>
+						<SunMoon />
 					</Button>
 				</Navbar>
 			</footer>
