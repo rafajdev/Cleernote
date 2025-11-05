8b683cda13af533e44debe4d90960433e7a9b7fa
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-05 16:25:05 -0300

    feat: Altera posicionamento dos filhos da div principal

--- src/pages/NoteList.tsx
+++ src/pages/NoteList.tsx
@@ -7,18 +7,10 @@ import { SunMoon } from 'lucide-react';
 export default function NoteList() {
 	return (
 		<div className='flex flex-col w-dvw h-dvh'>
-			<header className='flex items-center h-3/14 px-20'>
+			<header className='flex items-center justify-center h-3/14 px-20'>
 				<h1 className='text-3xl font-semibold'>Cleernote</h1>
 			</header>
-			<main className='flex flex-wrap gap-10 h-8/10 px-20'>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+			<main className='flex flex-wrap content-start justify-center gap-10 h-[80vh] overflow-y-auto overflow-x-hidden px-20'>
 				<Card />
 			</main>
 			<footer className='flex justify-center items-center h-3/14 px-20'>
