e133eed6d02bfa6468f4a7274d5f9c0f87e2e4ed
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-09 19:57:25 -0300

    feat: Adiciona tipos NoteType e NoteCreateType para gerenciamento de notas

---
+++ src/types/NoteType.ts
@@ -0,0 +1,9 @@
+export type NoteType = {
+	id: string;
+	title: string;
+	content: string;
+	createdAt: Date;
+	updatedAt: Date;
+};
+
+export type NoteCreateType = Omit<NoteType, 'id' | 'createdAt' | 'updatedAt'>;
