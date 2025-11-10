965bd754d43726e55b338cc34b1f3489215f99e7
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-09 21:29:15 -0300

    feat: Adiciona função para troca de estado da página

--- src/pages/NoteList.tsx
+++ src/pages/NoteList.tsx
@@ -3,8 +3,11 @@ import Card from '../components/Card';
 import Navbar from '../components/Navbar';
 import { Plus } from 'lucide-react';
 import { SunMoon } from 'lucide-react';
+import { useNotes } from '../hooks/useNotes';
 
 export default function NoteList() {
+	const { setIsOnEditPage } = useNotes();
+
 	return (
 		<div className='flex flex-col w-dvw h-dvh'>
 			<header className='flex items-center justify-center h-3/14 px-20'>
@@ -15,8 +18,8 @@ export default function NoteList() {
 			</main>
 			<footer className='flex justify-center items-center h-3/14 px-20'>
 				<Navbar>
-					<img src='/cleernote_icon.png' className='h-10'/>
-					<Button>
+					<img src='/cleernote_icon.png' className='h-10' />
+					<Button onClickFn={() => setIsOnEditPage(true)}>
 						<Plus />
 						New note
 					</Button>
