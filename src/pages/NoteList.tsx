ffe3997d696b79a069f62059191c51ed34dba8bb
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-10 23:43:23 -0300

    feat: Implementa design responsivo e fallback

--- src/pages/NoteList.tsx
+++ src/pages/NoteList.tsx
@@ -15,15 +15,19 @@ export default function NoteList() {
 
 	return (
 		<div className='flex flex-col w-dvw h-dvh'>
-			<header className='flex items-center justify-center h-3/14 px-20'>
+			<header className='flex items-center justify-center h-3/14 px-20 max-sm:px-10'>
 				<h1 className='text-3xl font-semibold'>Cleernote</h1>
 			</header>
-			<main className='flex flex-wrap content-start justify-center gap-10 h-[80vh] overflow-y-auto overflow-x-hidden px-20'>
-				{notes.map((note) => (
-					<Card id={note.id} title={note.title} content={note.content} />
-				))}
+			<main className='flex flex-wrap content-start justify-center gap-10 h-[80vh] overflow-y-auto overflow-x-hidden px-20 max-sm:px-10'>
+				{notes.length === 0 ? (
+					<p className='text-gray-500'>No notes yet</p>
+				) : (
+					notes.map((note) => (
+						<Card key={note.id} id={note.id} title={note.title} content={note.content} />
+					))
+				)}
 			</main>
-			<footer className='flex justify-center items-center h-3/14 px-20'>
+			<footer className='flex justify-center items-center h-3/14 px-20 max-sm:px-10'>
 				<Navbar>
 					<img src='/cleernote_icon.png' className='h-10' />
 					<Button onClickFn={handleOnClick}>
