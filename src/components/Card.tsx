6a8a6d079fc42e1675cc55493d1ecf01ca324cf7
rafajdev <rafadeoliveiramendes@gmail.com>
2025-11-10 21:35:45 -0300

    feat: Implementa botão e lógica para deletar a nota

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,4 @@
+import { Trash } from 'lucide-react';
 import { useNotes } from '../hooks/useNotes';
 
 type CardProps = {
@@ -7,29 +8,51 @@ type CardProps = {
 };
 
 export default function Card({ id, title, content }: CardProps) {
-	const { setIsOnEditPage, setCurrentNote } = useNotes();
+	const { setIsOnEditPage, setCurrentNote, deleteNote } = useNotes();
 
 	const handleOnClick = () => {
 		setCurrentNote({
-			id: id,
-			title: title,
-			content: content,
+			id,
+			title,
+			content,
 		});
 		setIsOnEditPage(true);
+	};
+
+	const handleDelete = (e: React.MouseEvent) => {
+		e.stopPropagation();
+		deleteNote(id);
 	};
 
 	return (
 		<div
 			className='
-         group
-         w-52 h-52 p-4 
-         border-2 border-gray-500 hover:border-gray-600 rounded-lg 
-         bg-white 
-         shadow-sm hover:shadow-lg transition-shadow
-         cursor-pointer
-         '
+        group relative
+        w-52 h-52 p-4 
+        border-2 border-gray-500 hover:border-gray-600 rounded-lg 
+        bg-white 
+        shadow-sm hover:shadow-lg transition-shadow
+        cursor-pointer
+      '
 			onClick={handleOnClick}
 		>
+			<button
+				onClick={handleDelete}
+				className='
+				absolute top-2 right-2
+				opacity-0 group-hover:opacity-100
+				pointer-events-none group-hover:pointer-events-auto
+				bg-red-500 text-white text-xs
+				w-8 h-8
+				flex items-center justify-center
+				rounded
+				transition-opacity
+				cursor-pointer
+			'
+			>
+				<Trash />
+			</button>
+
 			<h3 className='mb-2 text-lg font-semibold text-gray-700 group-hover:text-gray-800'>
 				{title}
 			</h3>
